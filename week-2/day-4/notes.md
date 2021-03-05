be00c38184a06449bcc73dd3297d74674226b569
cjsong29 <connersong29@gmail.com>
2021-03-04 18:43:46 -0800

    complete week-2

---
+++ week-2/day-4/notes.md
@@ -0,0 +1,14 @@
+2 ways to utilize jacascript with HTML (and css)
+
+answer: Script
+
+External JS FILE
+
+
+//Functions
+
+Syntanx
+
+function nameOfYourFunction (){
+    // do something
+}
---
+++ week-2/day-4/script.js
@@ -0,0 +1,23 @@
+alert("hello!");
+
+var nameItAnything = 1;
+var anotherThing = 1;
+
+// is nameItAnything = 1? yes
+// is nameItAnything = 2? no
+//anotherThing == nameItAnthing? yes
+
+var title = document.getElementById("grabMe");
+title.innerText = "hello!";
+
+function turnBGblue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+
+function changeTitleToNumber() {
+    var title = document.getElementById("grabMe");
+    title.innerText = nameItAnything;
+}
+
+changeTitleNumer ()
